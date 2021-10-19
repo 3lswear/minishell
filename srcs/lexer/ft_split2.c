3c4892913af12c7c3fb9e8f72615ec05eeaeb2eb
3lswear <r.malyavsky@protonmail.com>
2021-10-19 16:09:18 +0300

    refactor: clean up (remove i)

--- srcs/lexer/ft_split2.c
+++ srcs/lexer/ft_split2.c
@@ -2,17 +2,16 @@
 
 /* split string by delim, creating list of tokens,
  * adding flags to each and special_flg to a token with delim as a word */
-t_list	**ft_split2(char const *s, const char *delim, int flags, int special_flg)
+t_list	**ft_split2(char const *s, const char *delim, int flags,
+		int special_flg)
 {
-	int				len;
-	int delim_len;
-	int i;
-	char *substr;
-	t_list **list;
+	int		len;
+	int		delim_len;
+	char	*substr;
+	t_list	**list;
 
 	list = malloc(sizeof(t_list *));
 	*list = NULL;
-	i = 0;
 	len = ft_strlen(s);
 	delim_len = ft_strlen(delim);
 	substr = ft_strnstr(s, delim, len);
@@ -23,27 +22,27 @@ t_list	**ft_split2(char const *s, const char *delim, int flags, int special_flg)
 	}
 	while (s)
 	{
-		 substr = ft_strnstr(s, delim, len);
-		 if (substr)
-		 {
-			 /* if s contains ONLY delim */
-			 if (len == delim_len)
-				 ft_lstadd_back(list, ft_lstnew(wdesc_new(ft_substr(substr, 0, delim_len), flags | special_flg)));
-			 /* if there is a string BEFORE the delim */
-			 else
-			 {
-				 if (substr - &s[i])
-					 ft_lstadd_back(list, ft_lstnew(wdesc_new(ft_substr(s, 0, substr - &s[i]), flags | T_NOSPC)));
-				 ft_lstadd_back(list, ft_lstnew(wdesc_new(ft_substr(substr, 0, delim_len), flags | T_NOSPC | special_flg)));
-			 }
-			 s = substr + delim_len;
-		 }
-		 else
-		 {
-			 if (*s)
-				 ft_lstadd_back(list, ft_lstnew(wdesc_new(ft_substr(s, 0, len), flags)));
-			 break;
-		 }
+		substr = ft_strnstr(s, delim, len);
+		if (substr)
+		{
+			/* if s contains ONLY delim */
+			if (len == delim_len)
+				ft_lstadd_back(list, ft_lstnew(wdesc_new(ft_substr(substr, 0, delim_len), flags | special_flg)));
+			/* if there is a string BEFORE the delim */
+			else
+			{
+				if (substr - s)
+					ft_lstadd_back(list, ft_lstnew(wdesc_new(ft_substr(s, 0, substr - s), flags | T_NOSPC)));
+				ft_lstadd_back(list, ft_lstnew(wdesc_new(ft_substr(substr, 0, delim_len), flags | T_NOSPC | special_flg)));
+			}
+			s = substr + delim_len;
+		}
+		else
+		{
+			if (*s)
+				ft_lstadd_back(list, ft_lstnew(wdesc_new(ft_substr(s, 0, len), flags)));
+			break ;
+		}
 	}
 	return (list);
 }
