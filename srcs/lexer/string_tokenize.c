08e0f0d1058df2f91a92e91c9fe5b080268eacbc
3lswear <r.malyavsky@protonmail.com>
2021-10-21 17:25:15 +0300

    style(str_tokenize): norme

--- srcs/lexer/string_tokenize.c
+++ srcs/lexer/string_tokenize.c
@@ -1,13 +1,12 @@
 #include "minishell.h"
 
-
 void	split_on_special(t_list **tokens, t_list **delims, int spec_flg)
 {
-	t_list *li;
-	t_list *delim;
-	t_list *prev;
-	t_word_desc *word_desc;
-	t_list **split;
+	t_list		*li;
+	t_list		*delim;
+	t_list		*prev;
+	t_word_desc	*word_desc;
+	t_list		**split;
 
 	delim = *delims;
 	while (delim)
@@ -20,7 +19,8 @@ void	split_on_special(t_list **tokens, t_list **delims, int spec_flg)
 			if (word_desc->flags & (T_DQUOTE | T_NOEXP | T_SPEC))
 				split = NULL;
 			else
-				split = ft_split2(word_desc->word, delim->content, word_desc->flags, spec_flg);
+				split = ft_split2(word_desc->word, delim->content,
+						word_desc->flags, spec_flg);
 			tokens_insert(split, &li, &prev, tokens);
 			free(split);
 			li = li->next;
@@ -31,23 +31,19 @@ void	split_on_special(t_list **tokens, t_list **delims, int spec_flg)
 
 t_list **string_tokenize(t_minishell *state)
 {
-	t_list **tokens;
-	t_list *delims;
+	t_list	**tokens;
+	t_list	*delims;
 
 	delims = NULL;
-
 	tokens = first_pass(state->line, state, 0);
-
 	if (DEBUG)
 	{
 		fprintf(stderr, "=== after first_pass: ===\n");
 		word_list_print(tokens);
 	}
-
 	ft_lstadd_back(&delims, ft_lstnew("|"));
 	split_on_special(tokens, &delims, T_SPEC);
 	delims_free(&delims);
-
 	delims = NULL;
 	ft_lstadd_back(&delims, ft_lstnew("<<"));
 	ft_lstadd_back(&delims, ft_lstnew(">>"));
@@ -55,23 +51,18 @@ t_list **string_tokenize(t_minishell *state)
 	ft_lstadd_back(&delims, ft_lstnew("<"));
 	split_on_special(tokens, &delims, T_SPEC | T_REDIR);
 	delims_free(&delims);
-
 	if (DEBUG)
 	{
 		fprintf(stderr, "=== after split_on_special: ===\n");
 		word_list_print(tokens);
 	}
-
 	split_on_vars(tokens);
-
 	if (DEBUG)
 	{
 		fprintf(stderr, "=== after split_on_vars: ===\n");
 		word_list_print(tokens);
 	}
-
 	/* word_list_free(tokens); */
 	/* free(tokens); */
-
 	return (tokens);
 }
