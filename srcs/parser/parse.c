55b7a8fecdb70fdc90f27d34a4f46f2c708966d5
3lswear <r.malyavsky@protonmail.com>
2021-10-21 17:26:29 +0300

    style(parse): norme

--- srcs/parser/parse.c
+++ srcs/parser/parse.c
@@ -1,39 +1,35 @@
 #include "minishell.h"
 
-char *str_enlarge(char *orig, char *add)
+char	*str_enlarge(char *orig, char *add)
 {
-	size_t orig_len;
-	size_t add_len;
-	char *result;
+	size_t	orig_len;
+	size_t	add_len;
+	char	*result;
 
 	orig_len = ft_strlen(orig);
 	add_len = ft_strlen(add);
 	result = ft_calloc(orig_len + add_len + 1, sizeof(char));
 	ft_strlcpy(result, orig, orig_len + 1);
 	ft_strlcat(result, add, add_len + orig_len + 1);
-
 	free(orig);
 	return (result);
 }
 
-t_list **get_commands(t_list **tokens, t_minishell *mini)
+t_list	**get_commands(t_list **tokens, t_minishell *mini)
 {
-	t_command *cmd;
-	t_list **commands;
-	int next_cmd;
-	t_redirects cmd_redirs;
+	t_command	*cmd;
+	t_list		**commands;
+	int			next_cmd;
+	t_redirects	cmd_redirs;
 
 	next_cmd = 1;
-
 	commands = malloc(sizeof(t_list *));
 	*commands = NULL;
-	while(next_cmd && !(mini->exit_status))
+	while (next_cmd && !(mini->exit_status))
 	{
 		cmd = malloc(sizeof(t_command));
 		cmd->path = get_path(tokens);
-		/* fprintf(stderr, "==> current token = [%s]\n", ((t_word_desc *)((*tokens)->content))->word); */
 		cmd->arg = get_args(tokens, cmd->path);
-		/* fprintf(stderr, "==> token after args = [%s]\n", ((t_word_desc *)((*tokens)->content))->word); */
 		cmd_redirs = get_redir(tokens, mini);
 		cmd->red = cmd_redirs.redir;
 		cmd->append = cmd_redirs.append;
@@ -45,7 +41,6 @@ t_list **get_commands(t_list **tokens, t_minishell *mini)
 		validity_check(commands, mini);
 	if (DEBUG)
 		commands_print(commands);
-
 	return (commands);
 }
 
