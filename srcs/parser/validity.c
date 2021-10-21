cf7a02a6f07133c80cfd6f95b1728fba4a354edc
3lswear <r.malyavsky@protonmail.com>
2021-10-21 15:22:24 +0300

    style: normed

--- srcs/parser/validity.c
+++ srcs/parser/validity.c
@@ -2,23 +2,23 @@
 
 void	validity_check(t_list **cmds_list, t_minishell *mini)
 {
-	t_command *cmd;
-	t_list *li;
+	t_command	*cmd;
+	t_list		*li;
 
 	li = *cmds_list;
 	if (!li)
-		return;
+		return ;
 	while (li)
 	{
 		cmd = li->content;
-		if (!(cmd->path) && (!(cmd->red->in) && !(cmd->red->out) &&
-					!(cmd->append->in) && !(cmd->append->out))  && cmd->pipe)
+		if (!(cmd->path) && (!(cmd->red->in) && !(cmd->red->out)
+				&& !(cmd->append->in) && !(cmd->append->out)) && cmd->pipe)
 		{
 			handle_error(ERR_P_UNEXP, "|");
 			mini->exit_status = ERR_P_UNEXP;
 		}
-
+		if (mini->exit_status)
+			break ;
 		li = li->next;
 	}
-
 }
