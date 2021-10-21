def3b85034d5fd2012c9c00469d37267a3d38f7c
3lswear <r.malyavsky@protonmail.com>
2021-10-21 20:55:08 +0300

    refactor(tokens_merge): split

--- srcs/utils/tokens.c
+++ srcs/utils/tokens.c
@@ -1,5 +1,18 @@
 #include "minishell.h"
 
+static void	loop(t_list *token, t_list **tokens, char **result,
+		t_word_desc *word)
+{
+	while (token && (word->flags & T_NOSPC) && token->next
+		&& !(((t_word_desc *)token->next->content)->flags & T_SPEC))
+	{
+		token = token->next;
+		word = token->content;
+		*result = str_enlarge(*result, word->word);
+		*tokens = token->next;
+	}
+}
+
 char	*tokens_merge(t_list **tokens, t_flg stop_on)
 {
 	t_list		*token;
@@ -7,25 +20,15 @@ char	*tokens_merge(t_list **tokens, t_flg stop_on)
 	char		*result;
 
 	result = NULL;
-	(void)stop_on;
 	token = *tokens;
-	if (!token)
+	if (!token || (((t_word_desc *)token->content)->flags) & (T_SPEC | stop_on))
 		return (NULL);
 	word = token->content;
-	if (word->flags & T_SPEC)
-		return (NULL);
 	if ((word->flags & T_NOSPC) && token->next
 		&& !(((t_word_desc *)token->next->content)->flags & T_SPEC))
 	{
 		result = ft_strdup(word->word);
-		while (token && (word->flags & T_NOSPC) && token->next
-			&& !(((t_word_desc *)token->next->content)->flags & T_SPEC))
-		{
-			token = token->next;
-			word = token->content;
-			result = str_enlarge(result, word->word);
-			*tokens = token->next;
-		}
+		loop(token, tokens, &result, word);
 	}
 	else
 	{
