d9a59b7c132c9fed0e031bf0461db3a545deeb0a
Talyx <farruh.rahmatov40@gmail.com>
2021-10-21 20:43:02 +0300

    norm read_line.c file

--- srcs/lexer/read_line.c
+++ srcs/lexer/read_line.c
@@ -14,7 +14,7 @@
 
 int	read_line(t_minishell *mini)
 {
-	const char *prompt;
+	const char	*prompt;
 
 	signal(SIGINT, sig_handler);
 	signal(SIGQUIT, sig_handler);
@@ -28,11 +28,10 @@ int	read_line(t_minishell *mini)
 		free(mini->line);
 		ft_putstr_fd("exit\n", 2);
 		exit(0);
-		return (1);
 	}
 	else if (ft_strlen(mini->line) == 0)
 		return (0);
-	else if(ft_strlen(mini->line) > 0)
+	else if (ft_strlen(mini->line) > 0)
 		add_history(mini->line);
 	return (1);
 }
