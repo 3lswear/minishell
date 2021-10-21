f441613ea9f803c2c7c28c9932216180cb61486e
3lswear <r.malyavsky@protonmail.com>
2021-10-21 16:32:41 +0300

    refactor(first_pass): split and some logic clean-up

--- srcs/lexer/first_pass.c
+++ srcs/lexer/first_pass.c
@@ -102,62 +102,58 @@ void	scroll_ifs(char *str, int *i)
 	}
 }
 
-// splits by IFS, respecting quotes
-t_list	**first_pass(char *str, t_minishell *mini, int flag_add)
+static int	loop(int *i, char *str, t_list **tokens, int add)
 {
-	t_list **tokens;
-	int i;
-	int j;
-	char *ifs;
-	int error;
+	int	j;
+	int	error;
 
-	ifs = " \t\n";
+	error = 0;
+	j = *i;
+	while (str[j] && !in_set(&str[j], IFS) && str[j] != '\''
+		&& str[j] != '\"')
+		j++;
+	if (str[j] == '\'')
+	{
+		word_li_append(tokens, ft_substr(str, *i, j - *i), T_NOSPC | add);
+		error |= handle_quote(str, &j, tokens, 0);
+	}
+	else if (str[j] == '\"')
+	{
+		word_li_append(tokens, ft_substr(str, *i, j - *i), T_NOSPC | add);
+		error |= handle_dquote(str, &j, tokens, 0);
+	}
+	else
+		word_li_append(tokens, ft_substr(str, *i, j - *i), add);
+	*i = j;
+	return (error);
+}
 
-	tokens = malloc(sizeof(t_list *));
-	*tokens = NULL;
+// splits by IFS, respecting quotes
+t_list	**first_pass(char *str, t_minishell *mini, int add)
+{
+	t_list	**tokens;
+	int		i;
+	int		error;
 
+	tokens = ft_calloc(sizeof(t_list *), 1);
 	i = 0;
+	error = 0;
 	while (str[i])
 	{
-		/* while (str[i] && str[i] != '\'' && str[i] != '\"' && in_set(&str[i], ifs)) */
 		scroll_ifs(str, &i);
-		while (str[i])
+		while (str[i] && !error)
 		{
 			scroll_ifs(str, &i);
-			error = handle_quote(str, &i, tokens, flag_add);
-			if (error)
-				mini->exit_status = error;
-			error = handle_dquote(str, &i, tokens, flag_add);
-			if (error)
-				mini->exit_status = error;
+			error |= handle_quote(str, &i, tokens, add);
+			error |= handle_dquote(str, &i, tokens, add);
 			scroll_ifs(str, &i);
 			if (!str[i])
-				break;
-			j = i;
-			while (str[j] && !in_set(&str[j], ifs) && str[j] != '\'' && str[j] != '\"')
-				j++;
-			if (str[j] == '\'')
-			{
-
-				word_li_append(tokens, ft_substr(str, i, j - i), T_NOSPC | flag_add);
-				error = handle_quote(str, &j, tokens, 0);
-				if (error)
-					mini->exit_status = error;
-			}
-			else if (str[j] == '\"')
-			{
-				word_li_append(tokens, ft_substr(str, i, j - i), T_NOSPC | flag_add);
-				error = handle_dquote(str, &j, tokens, 0);
-				if (error)
-					mini->exit_status = error;
-			}
-			else
-				word_li_append(tokens, ft_substr(str, i, j - i), flag_add);
-			i = j;
-			if (!str[j])
-				break;
+				break ;
+			error = loop(&i, str, tokens, add);
 			scroll_ifs(str, &i);
 		}
+		if (error)
+			mini->exit_status = error;
 	}
 	return (tokens);
 }
