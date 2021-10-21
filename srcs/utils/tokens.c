430b531107ad3c4cf9a1c8099e794b87aacb1af8
3lswear <r.malyavsky@protonmail.com>
2021-10-21 17:29:41 +0300

    style(tokens): norme

--- srcs/utils/tokens.c
+++ srcs/utils/tokens.c
@@ -1,10 +1,10 @@
 #include "minishell.h"
 
-char *tokens_merge(t_list **tokens, t_flg stop_on)
+char	*tokens_merge(t_list **tokens, t_flg stop_on)
 {
-	t_list *token;
-	t_word_desc *word;
-	char *result;
+	t_list		*token;
+	t_word_desc	*word;
+	char		*result;
 
 	result = NULL;
 	(void)stop_on;
@@ -14,12 +14,12 @@ char *tokens_merge(t_list **tokens, t_flg stop_on)
 	word = token->content;
 	if (word->flags & T_SPEC)
 		return (NULL);
-	if ((word->flags & T_NOSPC) && token->next &&
-			!(((t_word_desc *)token->next->content)->flags & T_SPEC))
+	if ((word->flags & T_NOSPC) && token->next
+		&& !(((t_word_desc *)token->next->content)->flags & T_SPEC))
 	{
 		result = ft_strdup(word->word);
-		while (token && (word->flags & T_NOSPC) && token->next &&
-				!(((t_word_desc *)token->next->content)->flags & T_SPEC))
+		while (token && (word->flags & T_NOSPC) && token->next
+			&& !(((t_word_desc *)token->next->content)->flags & T_SPEC))
 		{
 			token = token->next;
 			word = token->content;
@@ -32,6 +32,5 @@ char *tokens_merge(t_list **tokens, t_flg stop_on)
 		result = ft_strdup(word->word);
 		*tokens = token->next;
 	}
-
 	return (result);
 }
