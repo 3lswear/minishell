fa145f067aefeda6415c103956a8c04f933d08fd
Talyx <farruh.rahmatov40@gmail.com>
2021-10-23 16:33:45 +0300

    Fixed all!

--- srcs/utils/fd_func.c
+++ srcs/utils/fd_func.c
@@ -4,6 +4,8 @@ void	init_fd(void)
 {
 	g_all_fd.fd_in = dup(0);
 	g_all_fd.fd_out = dup(1);
+	g_all_fd.redir_in = -1;
+	g_all_fd.redir_out = -1;
 	g_all_fd.fd_pipe_in = -1;
 	g_all_fd.fd_pipe_out = -1;
 	g_all_fd.pid = 2;
