85e8708c65499b5c9e6c39c6cb6587465ead8449
3lswear <r.malyavsky@protonmail.com>
2021-10-24 20:11:58 +0300

    fix(error): change codes to conform with bash (2)

--- includes/error.h
+++ includes/error.h
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 17:59:02 by sunderle          #+#    #+#             */
-/*   Updated: 2021/10/23 17:59:02 by sunderle         ###   ########.fr       */
+/*   Updated: 2021/10/24 20:03:41 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 # define ERROR_H
 # include "minishell.h"
 
-# define ERR_P_SYNTAX	100
+# define ERR_P_SYNTAX	2
 # define ERR_P_VAR		101
-# define ERR_P_UNEXP		102
-# define ERR_P_MISSING	103
-# define ERR_P_NOCMD		104
+# define ERR_P_UNEXP	2
+# define ERR_P_MISSING	2
+# define ERR_P_NOCMD	104
 # define ERR_P_NOREDOUT	105
 # define ERR_P_NOREDIN	106
 
